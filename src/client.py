3b99639d4aab4a586912a01b778a43fe96b78b0c
Patricia G Raftery <praftery8@gmail.com>
2018-04-02 16:01:49 -0700

    client and server work, msg sends, receives, and echos

--- src/client.py
+++ src/client.py
@@ -1,19 +1,40 @@
-# instructions
+# # instructions
 
-Construct a socket client that will accept a message from the command line and send it to a server
-You may need to review the docs for accepting command line arguments when running a script.
-When the server sends a response back, the client should accumulate the response and print it to the console.
+# Construct a socket client that will accept a message from the command line and send it to a server
+# You may need to review the docs for accepting command line arguments when running a script.
+# When the server sends a response back, the client should accumulate the response and print it to the console.
 
 
 # today
 
 import socket
-from datetime import date
+from datetime import datetime
 
+socket.getaddrinfo('127.0.0.1', 3000)
+infos = socket.getaddrinfo('127.0.0.1', 3000)
+len(infos)
+stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
+socket.getaddrinfo('127.0.0.1', 3000)
+stream_info
+client = socket.socket(*stream_info[:3])
+client
+client.connect(stream_info[-1])
+message = 'Hello World!'
+client.sendall(message.encode('utf8'))
 
+buffer_length = 8
+message_complete = False
+
+while not message_complete:
+    part = client.recv(buffer_length)
+    print(part.decode('utf8'))
+    if len(part) < buffer_length:
+        break
+
+client.close()
 
 # demo
-
+'''
 import socket
 socket.getaddrinfo('127.0.0.1', 3000)
 infos = socket.getaddrinfo('127.0.0.1', 3000)
@@ -39,3 +60,4 @@ while not message_complete:
         break
 client.close()
 history
+'''
--- src/server.py
+++ src/server.py
@@ -1,10 +1,10 @@
-# instructions
+# # instructions
 
-Construct a socket server that will accumulate a message from some client and return that exact message back to the client.
-When the server first starts, a message should be printed to the console of the format "--- Starting server on port 8888 at 12:43:08 29/01/2018 ---"
-When the server is stopped, the user shouldn't see any error message.
-When the server is stopped, a message should be printed to the console of the format --- Stopping server on port 8888 at 12:43:39 29/01/2018 ---
-Whenever a message is received by the server, the server prints to the console a log of that message. It doesn't have to match this exactly, but it should be something like: [12:43:10 29/01/2018] Echoed: 'Hello, world!'
+# Construct a socket server that will accumulate a message from some client and return that exact message back to the client.
+# When the server first starts, a message should be printed to the console of the format "--- Starting server on port 8888 at 12:43:08 29/01/2018 ---"
+# When the server is stopped, the user shouldn't see any error message.
+# When the server is stopped, a message should be printed to the console of the format --- Stopping server on port 8888 at 12:43:39 29/01/2018 ---
+# Whenever a message is received by the server, the server prints to the console a log of that message. It doesn't have to match this exactly, but it should be something like: [12:43:10 29/01/2018] Echoed: 'Hello, world!'
 
 
 # today
@@ -16,23 +16,55 @@ sock = socket.socket(
     socket.AF_INET,
     socket.SOCK_STREAM,
     socket.IPPROTO_TCP)
-sock
 PORT = 3000
 address = ('127.0.0.1', PORT)
 sock.bind(address)
-sock
-sock.listen(1)
-daytime = datetime.now().strftime("%H:%M:%S %d/%m/%y")
-print('--- Starting server on port {} at {} ---'.format(PORT, daytime))
-conn, addr = sock.accept()
+try:
+    sock.listen(1)
+    daytime = datetime.now().strftime("%H:%M:%S %d/%m/%y")
+    print('--- Starting server on port {} at {} ---'.format(PORT, daytime))
+    conn, addr = sock.accept()
 
+    buffer_length = 8
+    message_complete = False
+    message = b''
+    while not message_complete:
+        part = conn.recv(buffer_length)
+        message += part
+        # print(part.decode('utf8'))
+        if len(part) < buffer_length:
+            break
+    message = message.decode('utf8')
+    print(daytime, 'Echoed: ', message)
+    conn.sendall(message.encode('utf8'))
+    # while not message_complete:
+    #     part = conn.recv(buffer_length)
+    #     # write echo
+    #     print('[{}] Echoed: ', part.decode('utf8'))
+    #     if len(part) < buffer_length:
+    #         break
+
+    stopServer = datetime.now().strftime("%H:%M:%S %d/%m/%y")
+    print('--- Stopping server on port {} at {} ---'.format(PORT, stopServer))
+    conn.close()
+    sock.close()
+
+except KeyboardInterrupt:
+    try:
+        conn.close()
+    except NameError:
+        pass
+
+    sock.close()
+    stopServer = datetime.now().strftime("%H:%M:%S %d/%m/%y")
+    print('--- Stopping server on port {} at {} ---'.format(PORT, stopServer))
 
 
 
 
 
 # demo
-
+'''
 import socket
 sock = socket.socket(
     socket.AF_INET,
@@ -65,3 +97,4 @@ while not message_complete:
 conn.close()
 sock.close()
 history
+'''
